6e270882996f8632a3ebff308037feab0a0f19d1
babestvl <babestvl@gmail.com>
2018-06-05 17:03:08 +0900

    Random simple atom with numpy

---
+++ FluidGenerator.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+import sys
+
+def main():
+  px = np.random.uniform(-1, 1, size=(1, 10000))
+  py = np.random.uniform(-1, 1, size=(1, 10000))
+  pz = np.random.uniform(-1, 1, size=(1, 10000))
+  vx = np.random.random_integers(-1, 1, size=(1, 10000))
+  vy = np.random.random_integers(-1, 1, size=(1, 10000))
+  vz = np.random.random_integers(-1, 1, size=(1, 10000))
+  test_file = open("fluid.xyz","w")
+  test_file.write("{}\n{}\n".format(len(px[0]),"Comment:Atom"))
+
+  for i in range(10000):
+    test_file.write("{} {} {} {}\n".format("H",px[0][i],py[0][i],pz[0][i]))
+  test_file.close()
+
+if __name__ == "__main__":
+  main()
