9b775f4972ad62a4c598cee17c1972233f95ac74
babestvl <babestvl@gmail.com>
2018-06-06 14:08:25 +0900

    Update structure

--- FluidGenerator.py
+++ FluidGenerator.py
@@ -1,20 +1,14 @@
 import numpy as np
-import os
-import sys
 
-def main():
-  px = np.random.uniform(-1, 1, size=(1, 10000))
-  py = np.random.uniform(-1, 1, size=(1, 10000))
-  pz = np.random.uniform(-1, 1, size=(1, 10000))
-  vx = np.random.random_integers(-1, 1, size=(1, 10000))
-  vy = np.random.random_integers(-1, 1, size=(1, 10000))
-  vz = np.random.random_integers(-1, 1, size=(1, 10000))
-  test_file = open("fluid.xyz","w")
-  test_file.write("{}\n{}\n".format(len(px[0]),"Comment:Atom"))
+px = np.random.uniform(-0.5, 0.5, size=(1, 10000))
+py = np.random.uniform(-0.5, 0.5, size=(1, 10000))
+pz = np.random.uniform(-0.5, 0.5, size=(1, 10000))
+vx = np.random.random_integers(-1, 1, size=(1, 10000))
+vy = np.random.random_integers(-1, 1, size=(1, 10000))
+vz = np.random.random_integers(-1, 1, size=(1, 10000))
+test_file = open("fluid.xyz","w")
+test_file.write("{}\n\n".format(len(px[0])))
 
-  for i in range(10000):
-    test_file.write("{} {} {} {}\n".format("H",px[0][i],py[0][i],pz[0][i]))
-  test_file.close()
-
-if __name__ == "__main__":
-  main()
+for i in range(10000):
+  test_file.write("{} {} {} {} {}\n".format("H",px[0][i],py[0][i],pz[0][i],1))
+test_file.close()
--- Main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def main():
-  print(np.random.random(0, 10, size=(1,10)))
-
-if __name__ == "__main__":
-  main()
