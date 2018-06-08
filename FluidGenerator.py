9dcae2f4cbe52ce6e38ed276b0d6a4e5ded9c706
babestvl <babestvl@gmail.com>
2018-06-08 14:21:32 +0900

    Add space

--- FluidGenerator.py
+++ FluidGenerator.py
@@ -8,6 +8,7 @@ start_time = time.time()
 count = 0
 
 result_file = open("fluid.xyz","w")
+
 px = np.random.uniform(-1, 1, size=(1, 10000))
 py = np.random.uniform(-1, 1, size=(1, 10000))
 pz = np.random.uniform(-1, 1, size=(1, 10000))
@@ -15,14 +16,18 @@ vx = np.random.random_integers(-1, 1, size=(1, 10000))
 vy = np.random.random_integers(-1, 1, size=(1, 10000))
 vz = np.random.random_integers(-1, 1, size=(1, 10000))
 px,py,pz = px[0],py[0],pz[0]
+
 for i in range(10000):
   r = (px[i]**2)+(py[i]**2)+(pz[i]**2)
-  if r**(1/3) > 1:
+  r = r**(1/3)
+  if r > 1:
     px[i] = 0
     py[i] = 0
     pz[i] = 0
     count += 1
+
 result_file.write("{}\n{}\n".format(10000-count,1))
+
 for i in range(10000):
   if px[i] != 0:
     result_file.write("{} {} {} {}\n".format("H",px[i],py[i],pz[i]))
