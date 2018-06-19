be5edd16c9610d55511aae27052ba189d8a412a4
babestvl <babestvl@gmail.com>
2018-06-19 15:57:18 +0900

    Use verlet algo for calculation

--- ArgonSimulator.py
+++ ArgonSimulator.py
@@ -6,20 +6,21 @@ from Atom import Atom
 start_time = time.time()
 
 # Initial Values
-delta_time = 1 * np.power(10, -3) # 0.001 ps
+delta_time = 0.001 # ps
 mass_argon = 39.948
-mol = 6.022 * np.power(10, 23)
-sigma = 5.67 * np.power(10, -10)
+mol = 6.022 * (10**23)
+sigma = 3.4 * (10**-10)
+epsilon = 1.65 * 0.01
 T = 300
 R = 5
 border_const = 15
 # wrong value
 # Kb = 2.293 * np.power(10, -50)
 # Kb = 1.380 * np.power(10, -23)
-Kb = 2.293 * np.power(10, -10)
+Kb = 2.293 * (10**-10)
 
 # Initial Atoms
-atoms = [Atom() for i in range(5000)]
+atoms = [Atom() for i in range(1000)]
 
 # Delete unusable atoms
 usable_atoms = []
@@ -37,30 +38,25 @@ momentum_vector = mass_argon * velocity_vector
 
 # Apply Initial Velocity and Momentum to Direction
 for atom in atoms:
-  atom.setMomentumVector(momentum_vector)
-  atom.setVelocityVector(velocity_vector)
-
-def verletCalculation(atom):
-  momantum = momantum + 0.5 * delta_time * force
-  r = r + delta_time * momantum / mass_argon
-  force = forceCalculation(r)
-  momantum = + 0.5 * delta_time * force
-  return (momantum, r)
+  atom.setInitialMomentumVector(momentum_vector)
+  atom.setInitialVelocityVector(velocity_vector)
 
 def forceCalculation(r):
   pass
 
 def elasticBorder(atom):
   atom_r = atom.getR()
-  border_force = ((((-border_const) * (atom_r - R)) / atom_r) * atom.position_vector)
-  border_force *= delta_time
-  atom.momentum_vector += border_force
-  atom.velocity_vector += border_force/mass_argon
+  border_force = 0
+  if atom_r > R:
+    border_force = (((-border_const) * (atom_r - R)) / atom_r) * atom.position_vector
+  return border_force
 
-def update(atom):
-  if atom.getR() > R:
-    elasticBorder(atom)
-  atom.updatePositionVector()
+def verletCalculation(atom):
+  total_force = elasticBorder(atom)
+  atom.momentum_vector = atom.momentum_vector + (0.5 * delta_time * total_force)
+  atom.position_vector = atom.position_vector + delta_time * atom.momentum_vector / mass_argon
+  total_force = forceCalculation(atom.position_vector)
+  atom.momentum_vector = atom.momentum_vector + (0.5 * delta_time * total_force)
 
 result_file = open("sample_argon.xyz","w")
 
@@ -70,7 +66,7 @@ for i in range(1000):
   result_file.write("{}\n{}\n".format(len(atoms),1))
   for atom in atoms:
     result_file.write("{} {} {} {}\n".format("H", atom.position_vector[0], atom.position_vector[1], atom.position_vector[2]))
-    update(atom)
+    verletCalculation(atom)
 
 # -------------------------
 
--- Atom.py
+++ Atom.py
@@ -6,7 +6,6 @@ class Atom:
     self.direction = np.random.random_integers(-1, 1, 3)
     self.velocity_vector = 0
     self.momentum_vector = 0
-    self.force_vector = 0
     
   def getR(self):
     vector_square = np.power(self.position_vector,2)
@@ -14,12 +13,8 @@ class Atom:
     r = np.sqrt(r_square)
     return r
 
-  def setVelocityVector(self, velocity_vector):
+  def setInitialVelocityVector(self, velocity_vector):
     self.velocity_vector = self.direction * velocity_vector
 
-  def setMomentumVector(self, momentum_vector):
+  def setInitialMomentumVector(self, momentum_vector):
     self.momentum_vector = self.direction * momentum_vector
-
-  def updatePositionVector(self):
-    self.position_vector += self.momentum_vector
-    
