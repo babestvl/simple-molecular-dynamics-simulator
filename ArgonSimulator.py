906178c8680fc2acf8a2d642fc947c768d54078c
babestvl <babestvl@gmail.com>
2018-06-27 16:26:22 +0900

    Fix wrong sphere radius

--- ArgonSimulator.py
+++ ArgonSimulator.py
@@ -22,16 +22,16 @@ class Atom:
     self.momentum_vector = self.direction * momentum_vector
 
 # Initial Values
-delta_time = 0.003 # ps
+delta_time = 0.003
 mass_argon = 39.948
-mol = 6.022 * (10**23)
-sigma = 3.4 * (10**-10)
-epsilon = 1.65 * (10**-2)
+mol = 6.022 * (10 ** 23)
+sigma = 3.4 * (10 ** -10)
+epsilon = 1.65 * (10 ** -2)
 T = 300
-sphere_radius = 15
+sphere_radius = 30
 border_const = 5
-critical_distance = 20
-Kb = 8.3 * (10**-3)
+critical_distance = 15
+Kb = 8.3 * (10 ** -3)
 
 # Initial Atoms
 atoms = [Atom() for i in range(200)]
@@ -42,9 +42,6 @@ for atom in atoms:
   if atom.getDistance() <= sphere_radius:
     usable_atoms.append(atom)
 atoms = usable_atoms
-
-for i in range(1, len(atoms)):
-  atoms[i-1].id = i
 
 # Initial Velocity and Momentum
 random_velocity = np.random.uniform(-5, 5, 3)
@@ -58,15 +55,14 @@ for atom in atoms:
   atom.setInitialMomentumVector(momentum_vector)
 
 def forceCalculation(atom):
-  force = 0
-  for other in atoms: 
-    if atom.id != other.id:
+  for other in atoms:
+    if id(atom) != id(other):
       pos_diff = other.position_vector - atom.position_vector
       distance_square = np.sum(np.power(pos_diff, 2))
       distance = np.sqrt(distance_square)
       if distance <= critical_distance:
         tmp_cal = np.power(sigma, 6) / np.power(distance, 7)
-        force += 24 * epsilon * tmp_cal * (2 * tmp_cal + 1) * pos_diff
+        force = 24 * epsilon * tmp_cal * (2 * tmp_cal - 1) * pos_diff
         atom.force_vector += force
 
 def elasticBorder(atom):
@@ -90,7 +86,7 @@ for i in range(2000):
     if atom.getDistance() > sphere_radius:
       elasticBorder(atom)
     verletCalculation(atom)
-  if i%10==0:
+  if i%5==0:
     result_file.write("{}\n{}\n".format(len(atoms), 1))
     for atom in atoms:
       result_file.write("{} {} {} {}\n".format("Ar", atom.position_vector[0], atom.position_vector[1], atom.position_vector[2]))
